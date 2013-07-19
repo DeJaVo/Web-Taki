57d5f6ae4b1efee19c7a36935b44437f654f1d02
sheirabh@gmail.com <sheirabh@gmail.com@fb2e711e-9fc2-4b95-edba-c13fee675691>
2013-07-19 20:02:54 +0000

    I don't know

--- trunk/Taki/TakiModel.php
+++ trunk/Taki/TakiModel.php
@@ -4,46 +4,6 @@ include_once('DataBase.php');
 class taki_model
 {
     private $db;
-
-    /*function create_account($data)
-    {
-
-
-        if(isset($_POST['submit']))
-        {
-            $username = mysql_real_escape_string($_POST['username']);
-            $password1 = mysql_real_escape_string($_POST['password1']);
-            $password2 = mysql_real_escape_string($_POST['password2']);
-            $nickname = mysql_real_escape_string($_POST['nickname']);
-
-            //check to make sure that the username and password fields are not empty and that the passwords match
-            if(!empty($username) && !empty($password1) && !empty($password2) &&   ($password1 == $password2)&&!empty($nickname))
-            {
-                //make sure the username does not already exist, create sql query to check username
-                $query = "SELECT * FROM Players WHERE username = '$username' ";
-                $data=mysql_query($query);
-                //check to make sure no username row exists in the data, if data is empty or zero
-                if(mysql_num_rows($data) == 0)
-                {
-
-                }
-                else
-                {
-
-                }
-            }
-            elseif ($password1 != $password2)
-            {
-
-            }
-
-            else
-            {
-
-            }
-        }
-    }*/
-
     //C'tor
     public function taki_model()
     {
