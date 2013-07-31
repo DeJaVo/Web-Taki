acb8f8470e04a07d47e4305ecf8268f81e0eedb3
sheirabh@gmail.com <sheirabh@gmail.com@fb2e711e-9fc2-4b95-edba-c13fee675691>
2013-07-31 22:44:22 +0000

    bla blabla

--- trunk/Taki/game_view_functions.js
+++ trunk/Taki/game_view_functions.js
@@ -1,9 +1,52 @@
 //functions we need:
+//on_surrender
 //on_put_down_cards    //animate the movement only if turn is legal
 //on_deck
 //on_color
 //on_card select/unselect
+//parse_answer    // if answer is illegal/fatal error/etc...                                       V
 //build string to server
+//activate player ui
+//deactivate player ui
+//activate color menu
+
+
+function game_state(param_list)
+{
+    for(var i= 0;i<param_list.length;i=i+2)
+    {
+        var key = param_list[i];
+        var val = param_list[i+1];
+        this[key]= val;
+    }
+}
+
+//parse a str representing the server's answer
+//server returns:
+//0 - internal error
+//1 - illegal move
+//2 - ok new-game-status
+//3 - change color
+//4 - game-ended game-finish-status
+//5 - player lost
+//6 - player wins
+function parse_string(str) {
+    var params_array= new Array();
+    var result= str.charAt(0);
+    if ((result == 0) ||(result==1) || (result==3) || (result==5) || (result==6)) {return result;}
+    var game_state = str.slice(3,(str.length-1));
+    var params = game_state.split("&");
+    for (var i=0;i<params.length;i++) {
+        var param=params[i];
+        var key_val= param.split("=");
+        var key= key_val[0];
+        var val=key_val[1];
+        params_array.push(key, val);
+    }
+    return params_array;
+}
+
+
 
 var xmlhttp;
 function load_f(url,arg,cfunc)
