872674efa59a0cf59c4a1734dea7c75bf2ee0bd3
sheirabh@gmail.com <sheirabh@gmail.com@fb2e711e-9fc2-4b95-edba-c13fee675691>
2013-07-31 18:27:42 +0000

    bla blabla

---
+++ trunk/Taki/game_view_functions.js
@@ -0,0 +1,13 @@
+//functions we need:
+//on_surrender
+//on_put_down_cards    //animate the movement only if turn is legal
+//on_deck
+//on_color
+//on_card select/unselect
+//parse_answer    // if answer is illegal/fatal error/etc...
+//build string to server
+//activate player ui
+//diactivate player ui
+//activate color menu
+
+
